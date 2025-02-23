b806e1013d18ad162344fc0e2c1cbdb6d6e9c2eb
kapybotstu <mvenegas@jobby.cl>
2025-02-23 20:17:16 -0300

    xd5

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,13 @@
 import typescript from 'rollup-plugin-typescript2'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default {
-  input: 'src/app.ts',
-  output: {
-    file: 'dist/app.js',
-    format: 'esm'
-  },
-  plugins: [
-    nodeResolve(),
-    typescript({
-      tsconfig: 'tsconfig.json',
-      useTsconfigDeclarationDir: true
-    })
-  ],
-  external: [
-    '@builderbot/bot',
-    '@builderbot/provider-baileys',
-    'dotenv',
-    'fs',
-    'path'
-  ]
+    input: 'src/app.ts',
+    output: {
+        file: 'dist/app.js',
+        format: 'esm',
+    },
+    onwarn: (warning) => {
+        if (warning.code === 'UNRESOLVED_IMPORT') return
+    },
+    plugins: [typescript()],
 }
